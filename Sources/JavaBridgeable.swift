5d147a2cd4efab29835f0e360ef3fcbdd8a72d62
Andrew Druk <adruk@readdle.com>
2018-01-18 15:46:18 +0200

    Dev: add support Excpetion<-->Error and String direct implementation of JavaBridgeable

--- Sources/JavaBridgeable.swift
+++ Sources/JavaBridgeable.swift
@@ -44,3 +44,97 @@ extension Set: JavaBridgeable {}
 extension Date: JavaBridgeable {}
 extension Data: JavaBridgeable {}
 extension URL: JavaBridgeable {}
+
+extension String: JavaBridgeable {
+
+    public static func from(javaObject: jobject) throws -> String {
+        var isCopy: jboolean = 0
+        if let chars = JNI.api.GetStringChars( JNI.env, javaObject, &isCopy) {
+            defer {
+                if isCopy != 0 {
+                    JNI.api.ReleaseStringChars( JNI.env, javaObject, chars)
+                }
+            }
+            return String(utf16CodeUnits: chars, count: Int(JNI.api.GetStringLength(JNI.env, javaObject)))
+        }
+        else {
+            throw JavaCodingError.cantCreateObject("String")
+        }
+    }
+
+    public func javaObject() throws -> jobject {
+        guard let javaObject: jstring =  Array(utf16).withUnsafeBufferPointer({
+            JNI.api.NewString(JNI.env, $0.baseAddress, jsize($0.count))
+        }) else {
+            throw JavaCodingError.cantCreateObject("String")
+        }
+        return javaObject
+    }
+
+}
+
+// Error can't implement JavaBridgeable protocol
+fileprivate let javaExceptionClass = JNI.GlobalFindClass("java/lang/Exception")!
+fileprivate let javaExceptionConstructor = try! JNI.getJavaMethod(forClass: "java/lang/Exception", method: "<init>", sig: "(Ljava/lang/String;)V")
+fileprivate let javaExceptionGetMessage = try! JNI.getJavaMethod(forClass: "java/lang/Exception", method: "getMessage", sig: "()Ljava/lang/String;")
+
+extension Error {
+
+    public static func from(javaObject: jobject) throws -> Error {
+        let domain: String
+        let code: String
+        if let javaMessage = JNI.CallObjectMethod(javaObject, methodID: javaExceptionGetMessage) {
+            let message = try String.from(javaObject: javaMessage)
+            let parts = message.split(separator: ":")
+            domain = parts.count > 0 ? String(parts[0]) : "JavaException"
+            code = parts.count > 1 ? String(parts[1]) : "0"
+        }
+        else {
+            domain = "JavaException"
+            code = "0"
+        }
+        return NSError(domain: domain, code: Int(code) ?? 0)
+    }
+
+    public func javaObject() throws -> jobject {
+        let message: String
+        if let nsError = self as? NSError {
+            message = "\(nsError.domain):\(nsError.code)"
+        }
+        else {
+            message = String(reflecting: type(of: self))
+        }
+
+        guard let javaObject = JNI.NewObject(javaExceptionClass, methodID: javaExceptionConstructor, args: [jvalue(l: try message.javaObject())]) else {
+            throw JavaCodingError.cantCreateObject("java/lang/Exception")
+        }
+        return javaObject
+    }
+
+}
+
+extension Error where Self: RawRepresentable, Self.RawValue: SignedInteger {
+
+    public func javaObject() throws -> jobject {
+        let domain = String(reflecting: type(of: self))
+        let code: Int = numericCast(self.rawValue)
+        let message = try "\(domain):\(code)".javaObject()
+        guard let javaObject = JNI.NewObject(javaExceptionClass, methodID: javaExceptionConstructor, args: [jvalue(l: message)]) else {
+            throw JavaCodingError.cantCreateObject("java/lang/Exception")
+        }
+        return javaObject
+    }
+}
+
+extension Error where Self: RawRepresentable, Self.RawValue: UnsignedInteger {
+
+    public func javaObject() throws -> jobject {
+        let domain = String(reflecting: type(of: self))
+        let code: Int = numericCast(self.rawValue)
+        let message = try "\(domain):\(code)".javaObject()
+        guard let javaObject = JNI.NewObject(javaExceptionClass, methodID: javaExceptionConstructor, args: [jvalue(l: message)]) else {
+            throw JavaCodingError.cantCreateObject("java/lang/Exception")
+        }
+        return javaObject
+    }
+}
