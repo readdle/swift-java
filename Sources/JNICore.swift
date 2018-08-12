94f1830be0af5f4237f24a39dc3b641c572716fa
Andrew Druk <adruk@readdle.com>
2018-08-12 16:42:30 +0300

    Dev: move JNI_OnLoad to compiler

--- Sources/JNICore.swift
+++ Sources/JNICore.swift
@@ -13,46 +13,18 @@
 import Dispatch
 import Foundation
 
-@_silgen_name("JNI_OnLoad")
-public func JNI_OnLoad(jvm: UnsafeMutablePointer<JavaVM?>, ptr: UnsafeRawPointer) -> jint {
-    JNI.jvm = jvm
-    let env: UnsafeMutablePointer<JNIEnv?>? = JNI.GetEnv()
-    JNI.api = env!.pointee!.pointee
-    JNI.envCache[JNI.threadKey] = env
-#if os(Android)
-    DispatchQueue.setThreadDetachCallback(JNI_DetachCurrentThread)
-#endif
-
-    // Save ContextClassLoader for FindClass usage
-    // When a thread is attached to the VM, the context class loader is the bootstrap loader.
-    // https://docs.oracle.com/javase/1.5.0/docs/guide/jni/spec/invocation.html
-    // https://developer.android.com/training/articles/perf-jni.html#faq_FindClass
-    let threadClass = JNI.api.FindClass(env, "java/lang/Thread")
-    let currentThreadMethodID = JNI.api.GetStaticMethodID(env, threadClass, "currentThread", "()Ljava/lang/Thread;")
-    let getContextClassLoaderMethodID = JNI.api.GetMethodID(env, threadClass, "getContextClassLoader", "()Ljava/lang/ClassLoader;")
-    let currentThread = JNI.api.CallStaticObjectMethodA(env, threadClass, currentThreadMethodID, nil)
-    JNI.classLoader = JNI.api.NewGlobalRef(env, JNI.api.CallObjectMethodA(env, currentThread, getContextClassLoaderMethodID, nil))
-    let classLoaderClass = JNI.api.FindClass(env, "java/lang/ClassLoader")
-    JNI.loadClassMethodID = JNI.api.GetMethodID(env, classLoaderClass, "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;")
-
-    return jint(JNI_VERSION_1_6)
+public var JNI: JNICore {
+    return JNICore.instance
 }
-
-public func JNI_DetachCurrentThread() {
-    _ = JNI.jvm?.pointee?.pointee.DetachCurrentThread(JNI.jvm)
-    JNI.envLock.lock()
-    JNI.envCache[JNI.threadKey] = nil
-    JNI.envLock.unlock()
-}
-
-public let JNI = JNICore()
 
 open class JNICore {
 
-    open var jvm: UnsafeMutablePointer<JavaVM?>?
-    open var api: JNINativeInterface_!
-    open var classLoader: jclass!
-    open var loadClassMethodID: jmethodID!
+    public private(set) static var instance: JNICore!
+
+    open let jvm: UnsafeMutablePointer<JavaVM?>
+    open let api: JNINativeInterface_
+    open let classLoader: jclass
+    open let loadClassMethodID: jmethodID
 
     open var envCache = [pthread_t: UnsafeMutablePointer<JNIEnv?>?]()
     fileprivate let envLock = NSLock()
@@ -74,10 +46,50 @@ open class JNICore {
         return env
     }
 
+    public init(withJVM javaVM: UnsafeMutablePointer<JavaVM?>) {
+        var env: UnsafeMutablePointer<JNIEnv?>?
+
+        if withUnsafeMutablePointer(to: &env, {
+            $0.withMemoryRebound(to: UnsafeMutableRawPointer?.self, capacity: 1) {
+                javaVM.pointee?.pointee.GetEnv(javaVM, $0, jint(JNI_VERSION_1_6))
+            }
+        }) != jint(JNI_OK) {
+            fatalError("Unable to get initial JNIEnv")
+        }
+
+        let localThreadKey = pthread_self()
+
+        self.jvm = javaVM
+        self.api = env!.pointee!.pointee
+        self.envCache[localThreadKey] = env
+#if os(Android)
+        DispatchQueue.setThreadDetachCallback(JNI_DetachCurrentThread)
+#endif
+
+        // Save ContextClassLoader for FindClass usage
+        // When a thread is attached to the VM, the context class loader is the bootstrap loader.
+        // https://docs.oracle.com/javase/1.5.0/docs/guide/jni/spec/invocation.html
+        // https://developer.android.com/training/articles/perf-jni.html#faq_FindClass
+        let threadClass = api.FindClass(env, "java/lang/Thread")
+        let currentThreadMethodID = api.GetStaticMethodID(env, threadClass, "currentThread", "()Ljava/lang/Thread;")
+        let getContextClassLoaderMethodID = api.GetMethodID(env, threadClass, "getContextClassLoader", "()Ljava/lang/ClassLoader;")
+        let currentThread = api.CallStaticObjectMethodA(env, threadClass, currentThreadMethodID, nil)
+        guard let classLoader = api.NewGlobalRef(env, api.CallObjectMethodA(env, currentThread, getContextClassLoaderMethodID, nil)),
+              let classLoaderClass = api.FindClass(env, "java/lang/ClassLoader"),
+              let loadClassMethodID = api.GetMethodID(env, classLoaderClass, "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;") else {
+            fatalError()
+        }
+
+        self.classLoader = classLoader
+        self.loadClassMethodID = loadClassMethodID
+
+        JNICore.instance = self
+    }
+
     open func AttachCurrentThread() -> UnsafeMutablePointer<JNIEnv?>? {
         var tenv: UnsafeMutablePointer<JNIEnv?>?
         if withPointerToRawPointer(to: &tenv, {
-            self.jvm?.pointee?.pointee.AttachCurrentThread(self.jvm, $0, nil)
+            self.jvm.pointee?.pointee.AttachCurrentThread(self.jvm, $0, nil)
         }) != jint(JNI_OK) {
             report("Could not attach to background jvm")
         }
@@ -86,7 +98,7 @@ open class JNICore {
 
     open func report(_ msg: String, _ file: StaticString = #file, _ line: Int = #line) {
         NSLog("\(msg) - at \(file):\(line)")
-        if api?.ExceptionCheck(env) != 0 {
+        if api.ExceptionCheck(env) != 0 {
             api.ExceptionDescribe(env)
         }
     }
@@ -103,7 +115,7 @@ open class JNICore {
     open func GetEnv() -> UnsafeMutablePointer<JNIEnv?>? {
         var tenv: UnsafeMutablePointer<JNIEnv?>?
         if withPointerToRawPointer(to: &tenv, {
-            JNI.jvm?.pointee?.pointee.GetEnv(JNI.jvm, $0, jint(JNI_VERSION_1_6))
+            self.jvm.pointee?.pointee.GetEnv(self.jvm, $0, jint(JNI_VERSION_1_6))
         }) != jint(JNI_OK) {
             report("Unable to get initial JNIEnv")
         }
@@ -112,7 +124,7 @@ open class JNICore {
 
     open func FindClass(_ name: String, _ file: StaticString = #file, _ line: Int = #line) -> jclass? {
         ExceptionReset()
-        let clazz: jclass? = JNI.CallObjectMethod(classLoader, loadClassMethodID, name)
+        let clazz: jclass? = CallObjectMethod(classLoader, loadClassMethodID, name)
         if clazz == nil {
             report("Could not find class \(name)", file, line)
         }
@@ -163,4 +175,12 @@ open class JNICore {
         }
     }
 
+}
+
+// Callbacks for C API
+public func JNI_DetachCurrentThread() {
+    _ = JNI.jvm.pointee?.pointee.DetachCurrentThread(JNI.jvm)
+    JNI.envLock.lock()
+    JNI.envCache[JNI.threadKey] = nil
+    JNI.envLock.unlock()
 }
